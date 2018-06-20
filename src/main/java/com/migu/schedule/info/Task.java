8e3f25bb13ad028dd6a997659f66497778a53615
ipfs-cloud <1273498007@qq.com>
2018-06-20 20:59:40 +0800

    Update Task.java

--- src/main/java/com/migu/schedule/info/Task.java
+++ src/main/java/com/migu/schedule/info/Task.java
@@ -1,1 +1,47 @@
+package com.migu.schedule.info;
+
+/**
+ * 单个任务信息
+ * @author Administrator
+ *
+ */
+public class Task {
+	
+	private int taskId;
+	
+	private int consumption;
+	
+	
+
+	public Task() {
+		
+	}
+
+	public Task(int taskId, int consumption) {
+		this.taskId = taskId;
+		this.consumption = consumption;
+	}
+
+	public int getTaskId() {
+		return taskId;
+	}
+
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+
+	public int getConsumption() {
+		return consumption;
+	}
+
+	public void setConsumption(int consumption) {
+		this.consumption = consumption;
+	}
+
+	@Override
+	public String toString() {
+		return "Task [taskId=" + taskId + ", consumption=" + consumption + "]";
+	}
+	
+}
 
